d5bcc9a3779bdaab4f72df7d368a7557bb0d0dc7
quy1213 <trongquycoi11@gmail.com>
2025-02-10 12:31:07 +0700

    11

--- controller/controller.php
+++ controller/controller.php
@@ -5,8 +5,6 @@ include 'CategoryidC.php';
 include 'DetailC.php';
 include 'AuthC.php';
 include 'CategoryC.php';
-<<<<<<< HEAD
-=======
 include 'CartC.php';
 include 'OrderC.php';
->>>>>>> c892c6556ca3073b690b20c25219f5ab52c4fb8b
+
